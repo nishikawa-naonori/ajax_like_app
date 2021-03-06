66d5f52a7bccdd25985e4df51756183fe27e0453
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-03-06 13:06:02 +0900

    これで非同期完成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,16 @@
 class LikesController < ApplicationController
-   def create
+
+  def create
+    @blog = Blog.find(params[:blog_id])
     like = current_user.likes.build(blog_id: params[:blog_id])
     like.save
-    redirect_to blogs_path
+    redirect_to blogs_path #←この行を削除
   end
 
   def destroy
+    @blog = Blog.find(params[:blog_id])
     like = Like.find_by(blog_id: params[:blog_id], user_id: current_user.id)
     like.destroy
-    redirect_to blogs_path
+    redirect_to blogs_path #←この行を削除
   end
 end
